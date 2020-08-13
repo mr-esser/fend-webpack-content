338417564a4eb222b11187ab4d1e12a9bc4eecfa
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-08-13 20:48:57 +0200

    refactor: Add file-name to suffix

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,5 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker.js'
+import { handleSubmit } from './js/formHandler.js'
 
 console.log(checkForName);
 alert("I EXIST")
