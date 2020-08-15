93534504d00bfd77930e864ffd7b890a3b5a0d86
Martin Esser <64556948+mr-esser@users.noreply.github.com>
2020-08-15 21:33:55 +0200

    Build html via webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,30 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const htmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/client/index.js',
-  devtool: 'source-map',
+  mode: "development",
+  entry: "./src/client/index.js",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.js$/, // NO QUOTES!
-        enforce: 'pre',
+        enforce: "pre",
         exclude: /node_modules/,
-        use: [{loader: 'eslint-loader'}]
+        use: [{ loader: "eslint-loader" }],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: ['babel-loader']
-      }
-    ]
-  }
+        loader: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebPackPlugin({
+      template: "./src/client/views/index.html",
+      filename: "./index.html",
+    }),
+  ]
 };
